e61f4814cfc5379e0499cda745ee4d056b1d2e9d
Vlad <76412243+vldslt@users.noreply.github.com>
2022-05-24 21:52:31 +0300

    8.Advanced OOP 3
    8.Advanced OOP 3

---
+++ Cat.java
@@ -0,0 +1,33 @@
+package HomeWorkApp8;
+
+public class Cat implements Action {
+    private String name;
+    private int runDistance;
+    private int jumpHeight;
+
+    public Cat(String name, int distance, int jumpHeight) {
+        this.name = name;
+        this.runDistance = distance;
+        this.jumpHeight = jumpHeight;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("кот " + this.name + " пробежал " + this.getRunDistance());
+    }
+
+    @Override
+    public void jump() {
+        System.out.println("кот " + this.name + " прыгнул " + this.getJumpHeight());
+    }
+
+    @Override
+    public int getRunDistance() {
+        return this.runDistance;
+    }
+
+    @Override
+    public int getJumpHeight() {
+        return this.jumpHeight;
+    }
+}
