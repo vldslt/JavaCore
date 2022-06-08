dacb0ff149a244760d247fbaef0ec86eaf77ec91
Vlad <76412243+vldslt@users.noreply.github.com>
2022-06-08 20:21:47 +0300

    10.Collections
    10.Collections

---
+++ Apple.java
@@ -0,0 +1,13 @@
+package HomeWorkApp10;
+
+public class Apple extends Fruit {
+
+    Apple() {
+        super(1.0f);
+    }
+
+    @Override
+    public String toString(){
+        return "яблоки";
+    }
+}
